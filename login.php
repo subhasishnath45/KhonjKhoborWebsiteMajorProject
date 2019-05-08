7e5e75195056e4c8f0440cf8745524f227660b7a
Subhasish Nath <subhasishnath45@users.noreply.github.com>
2019-05-08 19:48:18 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Welcome to Khonjkhobor &#8212; WordPress</title>
+	    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(http://dlive.in/khobor/khonjkhobor/wp-content/themes/khonjkhobor/img/khonjkhobor-new-logo.jpg);
+		    height: 100px;
+		    width: 100%;
+		    background-size: contain;
+		    background-repeat: no-repeat;
+		    padding-bottom: 30px;
+        }
+		p#backtoblog {
+		    background: #0106ff !important;
+		    height: 65px !important;
+		    height: auto !important;
+		}
+		form#loginform {
+		    background: #0106ff;
+		}
+		.login label {
+    color: #fe0000 !important;
+    font-size: 20px !important;
+		}
+.wp-core-ui .button-group.button-large .button, .wp-core-ui .button.button-large {
+    height: 30px !important;
+    line-height: 28px !important;
+    padding: 10px 18px !important;
+    font-size: 2em !important;
+    height: auto !important;
+    /* background: #f1f1f1; */
+    /* color: #8a8686; */
+}
+#login {
+    width: 400px !important;
+    padding: 3% 0 0 !important;
+    margin: auto;
+}
+a.backToHome {
+    background: #0106ff !important;
+    width: 100% !important;
+    text-align: center !important;
+    font-size: 2.5em !important;
+    /* display: flex; */
+    padding: 0 16.4% 0px !important;
+    color: #d1d2ff !important;
+    text-transform: uppercase;
+}
+    </style>
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='http://dlive.in/khobor/khonjkhobor/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.10' type='text/css' media='all' />
+<link rel='stylesheet' id='royal_bangla_keyboard_style-css'  href='http://dlive.in/khobor/khonjkhobor/wp-content/plugins/royal-bangla-keyboard/keyboard.style.css?ver=4.9.10' type='text/css' media='all' />
+<script type='text/javascript' src='http://dlive.in/khobor/khonjkhobor/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='http://dlive.in/khobor/khonjkhobor/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="http://dlive.in/khobor/khonjkhobor/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="http://dlive.in/khobor/khonjkhobor/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+<a href="http://dlive.in/khobor/khonjkhobor/wp-login.php?action=register">Register</a> | 	<a href="http://dlive.in/khobor/khonjkhobor/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://dlive.in/khobor/khonjkhobor/">&larr; Back to Welcome to Khonjkhobor</a></p>
+		
+	</div>
+
+	
+	<script type="text/javascript">
+    jQuery(document).ready(function($){
+        $("p#backtoblog a").text("Back to home").addClass("backToHome");
+    });
+</script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
